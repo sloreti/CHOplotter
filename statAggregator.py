4b2b26e13796fd77dccd3dc5d44d7c0859d933a8
sloreti <lhloreti@gmail.com>
2016-07-21 22:08:52 -0400

    Beginning of Procedure class. Ensures data correctness from spreadsheet.

--- statAggregator.py
+++ statAggregator.py
@@ -1,24 +1,103 @@
 #!/usr/bin/env python 
 
+import datetime
 import openpyxl
-
 import matplotlib.pyplot as plt
-plt.plot([1,2,3,4])
-plt.ylabel('some numbers')
-plt.show()
 
-# surgeries = {}
+class Procedure(object):
+    """docstring for Procedure"""
 
-# wb = openpyxl.load_workbook('ProceduresData.xlsx')
-# sheet = wb.active
-# addons = 0
+    def __init__(self, procParams):
 
-# for row in sheet.rows:
-#     surgery = row[0].value
-#     if surgery == 'Yes':
-#         addons += 1
+        self.date = procParams.date
+        self.day = procParams.day
+        self.schedStart = self.toDateTime(procParams.schedStart)
+        self.schedEnd = self.toDateTime(procParams.schedEnd)
+        self.schedLength = self.toDateTime(procParams.schedLength, delta=True)
+        self.inRoom = self.toDateTime(procParams.inRoom)
+        self.ready = self.toDateTime(procParams.ready)
+        self.procStart = self.toDateTime(procParams.procStart)
+        self.procEnd = self.toDateTime(procParams.procEnd)
+        self.outRoom = self.toDateTime(procParams.outRoom)
+        self.procDuration = self.toDateTime(procParams.procDuration, delta=True)
+        self.roomDuration = self.toDateTime(procParams.roomDuration, delta=True)
+        self.room = procParams.room
+        self.loc = procParams.loc
+        self.logNum = procParams.logNum
+        
+        self.ensureAllEntriesCorrect(procParams)
 
-# for i in surgeries.iteritems():
-#     print i
 
-print float(addons)/len(sheet.rows)
+    def ensureAllEntriesCorrect(self, procParams):
+        self.durationsAreCorrect(procParams)
+        self.dayIsCorrect(procParams)
+    
+    def toDateTime(self, s, delta=False):
+        if s:
+            s = int(s)
+            if not delta:
+                return datetime.time(s/100, s%100) # (hours, minutes)
+            else:
+                return datetime.timedelta(minutes = s)
+        else:
+            return None
+        
+    def durationsAreCorrect(self, procParams):
+
+        allDursGood = True
+        durations = [(procParams.schedStart, procParams.schedEnd, procParams.schedLength), 
+                    (procParams.inRoom, procParams.outRoom, procParams.roomDuration), 
+                    (procParams.procStart, procParams.procEnd, procParams.procDuration)]
+        for dur in durations:
+            try:
+                startHour, endHour = dur[0] / 100, dur[1] / 100
+                if (endHour < startHour): # if procedure went from one day to the next
+                    endHour = (24 - startHour) + endHour
+                    startHour = 0
+
+                startMin, endMin = dur[0] % 100, dur[1] % 100
+                calculatedDur = (endMin - startMin) + (endHour - startHour)*60
+                if calculatedDur != dur[2]:
+                    allDursGood = False
+                    print "Procedure " + str(self.logNum) + " has an incorrect duration length"
+            except TypeError as e:
+                print "Procedure " + str(self.logNum) + ": " +  e.message
+
+        return allDursGood
+
+    def dayIsCorrect(self, procParams):
+        days = {'Mon':0, 'Tue':1, 'Wed':2, 'Thu':3, 'Fri':4, 'Sat':5, 'Sun':6}
+        if days[procParams.day] != self.date.weekday():
+            print "Procedure " + str(self.logNum) + " has an incorrectly labeled day"
+            return False
+        else:
+            return True
+
+
+class ProcedureParams(object):
+    """docstring for ProcedureParams"""
+    def __init__(self, row):
+        self.date = row[1].value
+        self.day = row[2].value
+        self.schedStart = row[5].value
+        self.schedEnd = row[6].value
+        self.schedLength = row[7].value
+        self.inRoom = row[8].value
+        self.ready = row[9].value
+        self.procStart = row[10].value
+        self.procEnd = row[11].value
+        self.outRoom = row[12].value
+        self.procDuration = row[13].value
+        self.roomDuration = row[14].value
+        self.room = row[15].value
+        self.loc = row[16].value
+        self.logNum = row[18].value
+
+
+wb = openpyxl.load_workbook('ProceduresData.xlsx')
+sheet = wb.active
+
+for row in sheet.rows[1:]:
+    params = ProcedureParams(row)
+    Procedure(params)
+
